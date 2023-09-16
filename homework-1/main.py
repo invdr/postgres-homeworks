c5174003b1cab831527403c47bb27443eb67275f
Home PC <homepc@mail.ru>
2023-09-16 21:13:31 +0300

    упрощение кода, typing, комменты

--- homework-1/main.py
+++ homework-1/main.py
@@ -4,9 +4,11 @@ import csv
 import psycopg2
 from psycopg2 import Error
 
-csv_files = ["north_data/customers_data.csv",
-             "north_data/employees_data.csv",
-             "north_data/orders_data.csv"]
+data_dict = {
+    "customers": "north_data/customers_data.csv",
+    "employees": "north_data/employees_data.csv",
+    "orders": "north_data/orders_data.csv"
+}
 connect_params = {
     "host": "localhost",
     "database": "north",
@@ -15,34 +17,26 @@ connect_params = {
 }
 
 
-def get_tables():
-    """Получаем список таблиц из БД."""
-    with psycopg2.connect(**connect_params) as conn:
-        with conn.cursor() as cur:
-            cur.execute("""SELECT relname FROM pg_class WHERE relkind='r'
-                           AND relname !~ '^(pg_|sql_)';""")
-
-            tables = [table[0] for table in cur.fetchall()][::-1]  # список таблиц в БД
-
-            return tables
-
-
-def get_csv_data(file_name):
+def get_csv_data(file_name: str) -> list:
     """Возвращает список данных из csv файла."""
     with open(file_name, newline='') as csvfile:
+        # создаем объект csv для работы
         reader = csv.reader(csvfile)
+        # пропускаем первую строку
         next(reader)
-        data = [tuple(line) for line in reader]
-        return data
+        return list(reader)
 
 
-def insert_data(connect, table, data):
+def insert_data(connect, table: str, data: list) -> None:
     """Запись данных в таблицу."""
-
+    # получаем кол-во %s по кол-ву столбцов в таблице
     values_count = ", ".join(["%s"] * len(data[0]))
     try:
+        # создаем курсор для работы
         with connect.cursor() as cur:
+            # получаем данные data в таблицу table
             cur.executemany(f"INSERT INTO {table} VALUES ({values_count})", data)
+            # заносим полученные данные в таблицу в pgAdmin
             connect.commit()
             print(f"Данные успешно внесены в таблицу {table.upper()}")
     except Error as e:
@@ -50,7 +44,10 @@ def insert_data(connect, table, data):
 
 
 # записать данных из csv файлов в соответствующие таблицы в pgAdmin
-for csv_file, current_table in zip(csv_files, get_tables()):
+for current_table, csv_file in data_dict.items():
+    # получаем данные для занесения в таблицу
     current_data = get_csv_data(csv_file)
+    # устанавливаем соединение с БД
     conn = psycopg2.connect(**connect_params)
+    # добавляем полученные данные в текущую таблицу
     insert_data(conn, current_table, current_data)
